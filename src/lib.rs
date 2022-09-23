4cecc8579c23187ea5e351b84a7e22df763f32a1
Tim White <tfy21twe@cs.umu.se>
2022-09-23 15:59:25 +0200

    worked on fn new, make move and declared functions for getting possible moves for each piece type

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
-use std::{fmt, io::Empty};
-
-// (1234b) means row1, column2 moves to row3, column4 and captures b
-//and a space owuld represent no capture
+use std::fmt;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 
@@ -14,99 +11,124 @@ pub enum GameState {
 pub enum Colour {
     White,
     Black,
-    NoColour,
 }
 
 pub struct Piece {
-    colour:Colour,
-    piece_type:PieceType,
+    colour: Colour,
+    piece_type: PieceType,
 }
 
 pub enum PieceType {
-    Empty, BlackPawn, BlackRook, BlackKnight, BlackBishop, BlackKing, BlackQueen, 
-    WhitePawn, WhiteRook, WhiteKnight, WhiteBishop, WhiteKing, WhiteQueen,
+    BlackPawn,
+    BlackRook,
+    BlackKnight,
+    BlackBishop,
+    BlackKing,
+    BlackQueen,
+    WhitePawn,
+    WhiteRook,
+    WhiteKnight,
+    WhiteBishop,
+    WhiteKing,
+    WhiteQueen,
 }
-
 
 pub struct Game {
     /* save board, active colour, ... */
-    board: [Piece;64],
+    board: [Option<Piece>; 64],
     active_colour: Colour,
-    state:GameState,
+    state: GameState,
 }
 
 impl Game {
     /// Initialises a new board with pieces.
-    pub fn new() -> Game { //0
+    pub fn new() -> Game {
+        //0
         use Colour::*;
         use PieceType::*;
 
-        let white_pawn = Piece {
-            colour:White,
-            piece_type:BP 
-        };
+        let white_pawn = Some(Piece {
+            colour: White,
+            piece_type: WhitePawn,
+        });
         //TODO continue this implementation
-        let white_rook = Piece {
-            colour:White,
-            piece_type:BP 
-        }; 
-        let white_pawn = Piece {
-            colour:White,
-            piece_type:BP 
-        }; 
-        let white_pawn = Piece {
-            colour:White,
-            piece_type:BP 
-        }; 
-        let white_pawn = Piece {
-            colour:White,
-            piece_type:BP 
-        }; 
-        let white_pawn = Piece {
-            colour:White,
-            piece_type:BP 
-        };
-        let empty_tile: Piece = Piece {
-            colour:Empty,
-            piece_type:E,
-        };
+        let white_rook = Some(Piece {
+            colour: White,
+            piece_type: WhiteRook,
+        });
+        let white_knight = Some(Piece {
+            colour: White,
+            piece_type: WhiteKnight,
+        });
+        let white_bishop = Some(Piece {
+            colour: White,
+            piece_type: WhiteBishop,
+        });
+        let white_king = Some(Piece {
+            colour: White,
+            piece_type: WhiteKing,
+        });
+        let white_queen = Some(Piece {
+            colour: White,
+            piece_type: WhiteQueen,
+        });
+        let black_pawn = Some(Piece {
+            colour: Black,
+            piece_type: BlackPawn,
+        });
+        let black_rook = Some(Piece {
+            colour: Black,
+            piece_type: BlackRook,
+        });
+        let black_knight = Some(Piece {
+            colour: Black,
+            piece_type: BlackKnight,
+        });
+        let black_bishop = Some(Piece {
+            colour: Black,
+            piece_type: BlackBishop,
+        });
+        let black_king = Some(Piece {
+            colour: Black,
+            piece_type: BlackKing,
+        });
+        let black_queen = Some(Piece {
+            colour: Black,
+            piece_type: BlackQueen,
+        });
 
         Game {
-            /* initialise board, set active colour to white, ... */
-            
-            //TODO fill board with correct piecetype
+            /* initialise board, set game state to in progress, set active colour to white, ... */
+            //TODO which order to store pieces
             board: [
-    
+                
             ],
-            state:GameState::InProgress,
-            active_colour:Colour::White,   
+            state: GameState::InProgress,
+            active_colour: Colour::White,
         }
     }
-
-    
 
     /// If the current game state is InProgress and the move is legal,
     /// move a piece and return the resulting state of the game.
     /// param: the position to move from and move to. Returns Option
     pub fn make_move(&mut self, from: String, to: String) -> Option<GameState> {
-        let mut vec: Vec<String> = Vec::with_capacity(60); //TODO what is dis
+        let mut vec: Vec<String> = Vec::with_capacity(60);
 
-        /* 
-        if get_possible_moves() == None {
+        //Check if the tile to move from is empty
+        if get_possible_moves(from) == None {
             println!("{}", "Illegal move, bruh");
             None
         }
 
-        possible_moves = get_possible_moves(from); 
-        let mut move_is_possible:bool;
-        
+        possible_moves = get_possible_moves(from);
+        let mut move_is_possible: bool;
+
         //check if tile to move to e.g. "to" is in possible_moves
         for possible_move in 0..possible_moves.len() {
             if possible_move == to {
                 move_is_possible = true;
-                continue;
-            }
-            else if possible_move == possible_moves.len() {
+                break;
+            } else if possible_move == possible_moves.len() {
                 move_is_possible = false;
             }
         }
@@ -114,32 +136,30 @@ impl Game {
         if !move_is_possible {
             println!("{}", "Illegal move, bruh");
             None
+        } else {
+            //move piece there
+        }
+        //check if it is checkmate/checkmate with get_game_state?
+        /*
+        if checkmate {
+            return GameOver
+        }get_ga
+        else if check {
+            //update active colour
+            return Check
         }
         else {
-            move piece there
-        }
-        check if it is checkmate/checkmate with get_game_state?
-            if checkmate {
-                return GameOver
-            }
-            else if check {
-                update active colour
-                return Check
-            }
-            else {
-                return InProgress
-            }
-        */ 
+            return InProgress
+        } */
     }
 
-    /// Set the piece type that a peasant becames following a promotion.
+    /// Set the piece type that a pawn turns into following a promotion.
     pub fn set_promotion(&mut self, piece: String) -> () {
         ()
     }
 
     //Get the current game state.
     pub fn get_game_state(&self) -> GameState {
-
         self.state
     }
 
@@ -147,82 +167,111 @@ impl Game {
     /// new positions of that piece.
     pub fn get_possible_moves(&self, tile_position: String) -> Option<Vec<String>> {
         use PieceType::*;
-        
-        let mut possible_moves: Vec<String> = Vec::new();
+
         let index = parse_inputted_tile_to_index(tile_position);
-        
+
         //TODO how to find piece on the board with the index and assign to variable piece_type
-        let piece: Piece = self.board[index];     
+        let piece = self.board[index].expect("Empty square"); //TODO handle None and Some
 
-        match piece.piece_type {
-            Empty => None,
-            BlackPawn => possible_moves = bk_possible_moves(index),
-        }
-
-        return possible_moves
-    }
-        
-        None
+        //Return the possible moves
+        Some(match piece.piece_type {
+            BlackPawn => black_pawn_possible_moves(index),
+            BlackRook => todo!(),
+            BlackKnight => todo!(),
+            BlackBishop => todo!(),
+            BlackKing => todo!(),
+            BlackQueen => todo!(),
+            WhitePawn => todo!(),
+            WhiteRook => todo!(),
+            WhiteKnight => todo!(),
+            WhiteBishop => todo!(),
+            WhiteKing => todo!(),
+            WhiteQueen => todo!(),
+        })
     }
 
     //Parse the letter and number in the tile name to the board index and return it
-    fn parse_inputted_tile_to_index(tile_position: String) -> i32 {
-
-        let mut col:i32;
+    pub fn parse_inputted_tile_to_index(tile_position: String) -> i32 {
+        let mut col: i32;
 
         //Convert the letter to correct column
-        /*   
+        /*
         match case with char at index 0 in tile name {
             "A" => col = 0,
             "B" => col = 1,
             "C" => col = 2,
-            "D" => col = 3, 
-            "E" => col = 4, 
+            "D" => col = 3,
+            "E" => col = 4,
             "F" => col = 5,
-            "H" => col = 6, 
+            "H" => col = 6,
             "G" => col = 7
-        }  
+        }
         */
-        
 
-        //Convert the number in the tile name to an int 
-        let mut row:i32;
+        //Convert the number in the tile name to an int
+        let mut row: i32;
 
-        /* 
-        could do this: let mut row = convert char at index 1 in tile_position to int.
-        Or could instead use match case if the above is complicated 
+        /*
+        use match case with 1 => 0 osv
         */
 
         //return the index
-        return row*8+col
-
+        return row * 8 + col;
     }
 
-    //Return possible moves in a vector of strings 
-    pub fn bk_possible_moves(i:i32) -> Option<Vec<i32>> { //
-        /* 
-        //TODO figure out logic for if king is at sides or corner. 
-         
+    ///
+    pub fn black_king_possible_moves(i: i32) -> Option<Vec<i32>> {
+        let col = i % 8;
+        row = i / 8;
+
+        /*
+
         Initialize list possible_tiles_to_move_to add possible tiles somehow
-            loop using index
+        loop using index
 
-        Initialize possible_moves 
-        
+        let mut possible_moves:Vec<i32> = Vec::new();
 
+        loop tile in possible_tiles
+            if col - 1 =< tile =< col + 1 && row -1 =< tile =< row + 1 && either is white or empty
+                possible_moves = tile
 
-        loop tile in possible_tiles 
-            if tile != 4 {
-                if 
+        if possible_moves isempty
+                    None
 
-                if tile has empty space or white piece && !(tile == i%8 || tile == i/8) {
-                    add to list of possible moves
-                }
-        
-        return vec with indexes of possible moves 
+        Some(possible_moves)
         */
+    }
 
-        let mut possible_moves:Vec<String> = Vec::new();
-    } 
+    ///
+    pub fn black_queen_possible_moves(i: i32) -> Option<Vec<i32>> {}
+
+    ///
+    /// # Arguments
+    /// 
+    /// 'i' - the index of the piece
+    /// 
+    /// ```
+    ///  
+    pub fn black_bishop_possible_moves(i: i32) -> Option<Vec<i32>> {}
+    ///
+    pub fn black_knight_possible_moves(i: i32) -> Option<Vec<i32>> {}
+    ///
+    pub fn black_rook_possible_moves(i: i32) -> Option<Vec<i32>> {}
+    ///
+    pub fn black_pawn_possible_moves(i: i32) -> Option<Vec<i32>> {}
+    ///
+    pub fn white_king_possible_moves(i: i32) -> Option<Vec<i32>> {}
+    ///
+    pub fn white_queen_possible_moves(i: i32) -> Option<Vec<i32>> {}
+    ///
+    pub fn white_bishop_possible_moves(i: i32) -> Option<Vec<i32>> {}
+
+    pub fn white_knight_possible_moves(i: i32) -> Option<Vec<i32>> {}
+
+    pub fn white_rook_possible_moves(i: i32) -> Option<Vec<i32>> {}
+
+    pub fn white_pawn_possible_moves(i: i32) -> Option<Vec<i32>> {}
+}
 
 /// Implement print routine for Game.
 ///
@@ -249,6 +298,11 @@ impl fmt::Debug for Game {
 // ######### TESTS ##########
 // --------------------------
 
+    /// ```
+    /// let x = 5;
+    /// 
+    /// asser_eq!(x == 5);
+    /// 
 #[cfg(test)]
 mod tests {
     use super::Game;
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,25 @@
 fn main() {
+    impl<T: Clone> Array2D<T> {}
 
+    pub fn from_rows(elements: &[Vec<T>]) -> Self {}
 
-    impl<T: Clone> Array2D<T> {
-
-        
-    }
-
-    pub fn from_rows(elements: &[Vec<T>]) -> Self {
-
-
-    }
-    
     //Unfinished visual board representation to test printing the board
     //correctly
     let board = [
-        "br", "bn", "bb", "bq", "bk", "bb", "bn", "br",
-        "bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp",
-        "e", "e", "e", "e", "e", "e", "e", "e",
-        "e", "e", "e", "e", "e", "e", "e", "e",
-        "e", "e", "e", "e", "e", "e", "e", "e",
-        "e", "e", "e", "e", "e", "e", "e", "e",
-        "wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp",
-        "wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"
-        ];
+        "br", "bn", "bb", "bq", "bk", "bb", "bn", "br", "bp", "bp", "bp", "bp", "bp", "bp", "bp",
+        "bp", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e",
+        "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "wp", "wp",
+        "wp", "wp", "wp", "wp", "wp", "wp", "wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr",
+    ];
 
     print_board(board);
 
     //TODO implement an array with board data
-
 }
 
 fn print_board(array: board) {
     //prints the initital board
-    //TODO fix this to print each line correctly with rank and file next to the board 
+    //TODO fix this to print each line correctly with rank and file next to the board
     for piece in board {
         println!("{}", piece)
     }
